9464438184221c450aafa63143b13ed53922f87f
MMMarz009 <mar0099887@gmail.com>
2020-06-30 14:21:27 -0500

    added logic but $.ajax is not recognized as a function. no problems in workspace

--- script.js
+++ script.js
@@ -1,64 +1,157 @@
 // Store previous locations in local storage in an array
 var prevLocations = [];
     // Have said locations populate id= savedSearches
+function start(){
+    prevLocations= JSON.parse(localStorage.getItem("weathercities"));
+}
+
+//show the prevLocations
+function prevLocationsShow  (){
+    if (prevLocations){
+        // make the btns for the previous locations
+        var btn = $("<div>").attri("class","list group");
+        for (var i=0; i< prevLocations.length; i++){
+            btnLocation= $("<a>").attri("href","#").attri("id", "btn").text(prevLocations[i]);
+            btnLocation.prepend(btn)
+        }
+    }
+    $("#savedSearches").append(btn);
+}
 
 // get weather data from user input for present day api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+function getWeather(city){
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7e3154185511d5ccdc5d2529706fdf8f&units=imperial";
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+       
+    }).then(function (response){
+      // create card with data in id = forecastData 
+        var cardCurrentCity= $("<div>").attr("class", "card bg-light");
+        $("#forecastData").append(cardCurrentCity);
 
-    // create card with data in id = forecastData 
+        // Head of card = location from user input 
+        var locationHead=$("<div>").attri("class","card-header").text("Current weather: " + response.name);
+        cardCurrentCity.append(locationHead);
 
-    // Head of card = location from user input 
+        //  row needs to be created to add icons 
+        var cardRows=$("<div>").attr("class", "row no-gutters");
+        cardCurrentCity.append(cardRows);
 
-    //  row needs to be created to add icons 
+        // get icons 
+        var icon=  "https://openweathermap.org/img/wn/" + response.weather[0].icon+"@2x.pg";
+        var imgIcon= $("<div>").attr("class","col-md-4").append($("<img>").attr("src",icon).attr("class","card-img"));
 
-    // get icons 
+        // add text space 
+        var textSpace = $("<div>").attr("class","col-md-8");
+        // add text space to the body of the card
+        var body= $("<div>").attr("class","card-body");
+        textSpace.append(body);
 
-    // add text space 
-
-    // add text space to the body of the card
-
-    // Insert the following required data 
+        // Insert the following required data 
         // city name 
+        body.append($("<h1>").attr("class","card-title").text(response.name));
 
         // temp in F so conversion 
+        body.append($("<p>").attr("class","card-text").html("Temp: "+response.main.temp + "&#8457;"));
 
         // Humidity in % 
+        body.append($("<p>").attr("class","card-text").text("Humidity: "+ response.main.humidity + "%"));
 
         // Wind speed in MPH 
+        body.append($("<p>").attr("class","card-text").text("Winds: "+ response.wind.speed+"MPH"));
+    });
 
-    // also required is the uv index from https://api.openweathermap.org/data/2.5/uvi + api key + lat + &lon= + long
+    cardRows.append(textSpace);
+    getWeather(response.id);
+    
+}
 
-        // get long 
 
-        // get lat 
-
-        // display onto the card body w/ colour
         
 // get the 5 day forecast from https://api.openweathermap.org/data/2.5/forecast?id= + city + api key w/ &units=imperial
-    // add container for the new cards 
-    // Go through an array to find forecasts for 12 
-        // make new card 
-        // add to card head with date & append
-        // add img & append 
-        // Add body & append to card 
-            // append with temp & humidity 
+function futureForecast(city) {
+    var queryURL= "https://api.openweathermap.org/data/2.5/forecast?id=" + city + "&appid=7e3154185511d5ccdc5d2529706fdf8f&units=imperial";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+         // add container for the new cards 
+         var rowNew = $("<div>").attr("class","forecast");
+         $("#forecastData").append(rowNew);
+      
+         // Go through an array to find forecasts for 14 hr 
+         for (var i=0 ; i< response.list.length; i++){
+            // make new card
+            var newColumn= $("<div>").attr("class", "one-fifth");
+            rowNew.append(newColumn);
+
+            // add to card head with date & append
+             var newCardSpace = $("<div>").attr("class", "card text-grey bg-primary");
+            newColumn.append(newCardSpace);
+            var dateHeader = $("<div>").attr("class", "card header").text(moment(response.list[i].dt, "x").format('L'));
+            newCardSpace.append(dateHeader);
+
+            // add img & append 
+            var imgCard= $("<img>").attr("class","card-img-top").attr("src", "https://openweathermap.org/img/wn/"+response.list[i].weather[0].icon + "@2x.png");
+            newCardSpace.append(imgCard);
+
+            // Add body & append to card 
+             var cardBody=$("<div>").attr("class","card-body");
+            newCardSpace.append(cardBody);
+            // append with temp & humidity to body 
+            cardBody.append($("<p>").attr("class", "card-text").html("Temp: "+response.list[i].main.temp+"F"));
+            cardBody.append($("<p>").attr("class", "card-text").text("Humidity: " + response.list[i].main.humidity + "%"));
+         }
+
+    });       
+        
+}
+
 
 
 // clear 
-
+function clear(){
+    $("#forecastData").empty();
+}
 
 // save prevLocations in local storage & push into array 
+function savePrev(locationsOld){
+   prevLocations.push(locationsOld);
+   localStorage.setItem("weathercities",JSON.stringify(prevLocations))
+   prevLocationsShow();
+}
+
+
+
 
 // click event on the id=btnSearch
-    // event default 
+$("#btnSearch").on("click",function(){
+    //event default 
+    event.preventDefault();
     // grab the input 
+    var location=$("#userSearch").val().trim();
     // IF NOT EMPTY then clear 
-// get weather data 
+    if (location !== ""){
+        clear();
+        $("#userSearch").val("");
+
+         // get weather data 
+        getWeather(location);
+
+    }
+
+});
+
 
 
 // only on search btn 
+$(document).on("click", "#btnSearch",function(){
+    clear();
+    prevLocationsShow();
+    getWeather();
+})
 
 
-// initialize 
-
-
+start();
 
