8fb5277fe618190a72de3d7b8fa9437143081234
MMMarz009 <mar0099887@gmail.com>
2020-06-29 19:41:42 -0500

    Added pseudo code & script to html

---
+++ script.js
@@ -0,0 +1,64 @@
+// Store previous locations in local storage in an array
+var prevLocations = [];
+    // Have said locations populate id= savedSearches
+
+// get weather data from user input for present day api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+
+    // create card with data in id = forecastData 
+
+    // Head of card = location from user input 
+
+    //  row needs to be created to add icons 
+
+    // get icons 
+
+    // add text space 
+
+    // add text space to the body of the card
+
+    // Insert the following required data 
+        // city name 
+
+        // temp in F so conversion 
+
+        // Humidity in % 
+
+        // Wind speed in MPH 
+
+    // also required is the uv index from https://api.openweathermap.org/data/2.5/uvi + api key + lat + &lon= + long
+
+        // get long 
+
+        // get lat 
+
+        // display onto the card body w/ colour
+        
+// get the 5 day forecast from https://api.openweathermap.org/data/2.5/forecast?id= + city + api key w/ &units=imperial
+    // add container for the new cards 
+    // Go through an array to find forecasts for 12 
+        // make new card 
+        // add to card head with date & append
+        // add img & append 
+        // Add body & append to card 
+            // append with temp & humidity 
+
+
+// clear 
+
+
+// save prevLocations in local storage & push into array 
+
+// click event on the id=btnSearch
+    // event default 
+    // grab the input 
+    // IF NOT EMPTY then clear 
+// get weather data 
+
+
+// only on search btn 
+
+
+// initialize 
+
+
+
